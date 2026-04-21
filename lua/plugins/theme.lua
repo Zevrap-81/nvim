1095c6e58bc533f2a10d6982ef53c154e3ecb68e
Parvez <p.mohammed@novum-engineering.com>
2026-04-21 16:43:04 +0200

    add catpuccin theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,30 @@
-/home/parvez/.config/omarchy/current/theme/neovim.lua
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = false,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mason = true,
+        mini = true,
+        native_lsp = { enabled = true },
+        noice = true,
+        notify = true,
+        snacks = true,
+        telescope = { enabled = true },
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
